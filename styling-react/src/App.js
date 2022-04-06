b62ac8aca78dba9bb2ab737c2097e76bb6b1330f
MinJ3453 <lmj34533453@gmail.com>
2022-04-06 19:25:36 +0900

    컴포넌트 스타일링 스터디

---
+++ styling-react/src/App.js
@@ -0,0 +1,16 @@
+import SassComponent from './SassComponent.js';
+import { Component } from 'react'
+
+class App extends Component{
+
+  render(){
+    return (
+      <div>
+       <SassComponent/>
+      </div>
+    );
+  }
+
+}
+
+export default App;
