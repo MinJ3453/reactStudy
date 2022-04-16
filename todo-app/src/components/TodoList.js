6aee5badc640455889d2d64178c110b5ecb9e212
MinJ3453 <lmj34533453@gmail.com>
2022-04-16 13:51:39 +0900

    11강 최적화 22/04/16

--- todo-app/src/components/TodoList.js
+++ todo-app/src/components/TodoList.js
@@ -1,16 +1,37 @@
+import React, { useCallback } from 'react';
+import {List} from 'react-virtualized';
 import TodoListItem from './TodoListItem';
 import './TodoList.scss';
 
 const TodoList = ({ todos, onRemove, onToggle }) =>{
-    return(
-        <div className='TodoList'>
-            {
-                todos.map(todo => (
-                    <TodoListItem todo={ todo } key={ todo.id } onRemove={onRemove} onToggle={onToggle}/>
-                ))
-            } 
-        </div>
+    const rowRenderer =useCallback(
+        ({idex, key, style}) => {
+            const todo =todos[idex];
+        
+            return(
+                <TodoListItem
+                    todo= {todo}
+                    key= {key}
+                    onRemove= {onRemove}
+                    onToggle={onToggle}
+                />
+            );
+        }, 
+        [onRemove,onToggle,todos],
     );
-}
 
-export default TodoList;
+    return(
+        <List
+         className='TodoList'
+         width={512}
+         height={513}
+         rowCount={todos.length}
+         rowHeight={57}
+         rowRenderer={rowRenderer}
+         list={todos}
+         style={{outline: 'none'}}
+        />
+    );
+};
+
+export default React.memo(TodoList);
